a7f3f5ea2dd15d263f1ebee32766c98718d388ec
Kien Nguyen <kien.x.ng@gmail.com>
2020-02-15 20:33:47 -0600

    upload events and requests API

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from wtforms import StringField, TextAreaField, PasswordField, DateTimeField, In
 # utility imports
 from functools import wraps
 import bcrypt
+import json
 
 app = Flask(__name__)
 
@@ -51,6 +52,7 @@ class Request(db.Document):
     name = db.StringField(db_field='name')
     time = db.DateTimeField(db_field='time')
     desc = db.StringField(db_field='desc')
+    host = db.ReferenceField(User, db_field='host_person')
 
 
 """ Forms """
@@ -88,25 +90,6 @@ class RegisterForm(FlaskForm):
     def validate_email(form, field):
         if User.objects.filter(email=field.data).first():
             raise validators.ValidationError('Email already exists')
-
-# event
-class EventForm(FlaskForm):
-    name = StringField('Name', [
-        validators.DataRequired(),
-        validators.length(min=4, max=50)
-    ])
-
-    time = DateTimeField('Time', [
-        validators.DataRequired()
-    ])
-
-    location = StringField('Location', [
-        validators.DataRequired()
-    ])
-
-    max_ppl = IntegerField('Max Participants')
-
-    desc = TextAreaField('Description')
 
 """ Routes """
 # register
@@ -168,85 +151,71 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
-# events
+# upload event
 @login_required
-@app.route('/event/upload', methods=['GET', 'POST'])
+@app.route('/event/upload', methods=['POST'])
 def upload_event():
     if 'username' in session:
-
-        form = EventForm()
-
         if request.method == 'POST':
-            if form.validate_on_submit():
-                new_event = Event()
-                new_event.name = form.name.data
-                new_event.time = form.time.data
-                new_event.location = form.location.data
-                
-                if form.max_ppl.data:
-                    new_event.max_participants = form.max_ppl.data
-
-                if form.desc.data:
-                    new_event.desc = form.desc.data
-
-                host = User.objects.get(username=session['username'])
-
-                if host is None:
-                    return abort(403)
-
-                new_event.host = host
-                new_event.save()
-
-                return redirect(url_for('index'))
-        return render_template('event/upload_event.html', form=form)
-    else:
-        return abort(403)
-
-# edit events
-@login_required
-@app.route('/event/edit/<id>', methods=['GET', 'POST'])
-def edit_event(id):
-    if 'username' in session:
-        try:
-            event = Event.objects.get(id=id)
-        except:
-            return abort(404)
-
-        user = User.objects.get(username=session['username'])
-
-        if user.id == event.host.id: # valid user to edit
-            form = EventForm(
-                name=event.name,
-                time=event.time,
-                location=event.location,
-            )
-
-            if event.max_participants:
-                form.max_ppl.data = event.max_participants
+            data = json.loads(request.data)
+            user = User.objects.get(username=session['username'])
             
-            if event.desc:
-                form.desc.data = event.desc
+            event = Event()
+            event.name = data['event-name']
+            event.time = data['event-time']
+            event.location = data['event-location']
+            event.max_participants = data['event-ppl']
+            event.desc = data['event-desc']
+            event.host = user
+            event.save()
 
-            if request.method == 'POST':
-                if form.validate_on_submit():
-
-                    event.name = form.name.data
-                    event.time = form.time.data
-                    event.location = form.location.data
-                    
-                    event.max_participants = form.max_ppl.data
-
-                    event.desc = form.desc.data
-                    print(form.desc.data)
-                    event.save()
-
-                    return redirect(url_for('index'))
-        else:
-            return abort(403)
-
-        return render_template('event/edit_event.html', form=form)
+            return {
+                "status": "success"
+            }
+        return {
+            "status": "fail"
+        }
     else:
         return abort(403)
+
+# all events
+@login_required
+@app.route('/events', methods=['GET'])
+def all_events():
+    events = Event.objects().to_json()
+    return events
+
+# upload request
+@login_required
+@app.route('/request/upload', methods=['POST'])
+def upload_request():
+    if 'username' in session:
+        if request.method == 'POST':
+            data = json.loads(request.data)
+            user = User.objects.get(username=session['username'])
+            
+            req = Request()
+            req.name = data['request-name']
+            req.time = data['request-time']
+            req.desc = data['request-desc']
+            req.host = user
+            req.save()
+
+            return {
+                "status": "success"
+            }
+        return {
+            "status": "fail"
+        }
+    else:
+        return abort(403)
+
+# all requests
+@login_required
+@app.route('/requests', methods=['GET'])
+def all_requests():
+    requests = Request.objects().to_json()
+    return requests
 
 # general routes
 @app.route('/', methods=['GET'])
